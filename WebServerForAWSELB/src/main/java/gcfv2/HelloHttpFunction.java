0cf224319ebf48e143aba72706221e5be91f476f
Oleksandr_Shlykov <oshlykov@pmintl.net>
2023-07-17 09:13:21 +0200

    many changes

---
+++ WebServerForAWSELB/src/main/java/gcfv2/HelloHttpFunction.java
@@ -0,0 +1,15 @@
+package gcfv2;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import java.io.BufferedWriter;
+
+public class HelloHttpFunction implements HttpFunction {
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws Exception {
+            final BufferedWriter writer = response.getWriter();
+            writer.write("Remote Hello world!");
+    }
+}
